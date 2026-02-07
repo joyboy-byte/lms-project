24f9c34d3d4ec34de956fa04618a3eff69bacc75
joyboy-byte <vyash5491@gmail.com>
2026-02-08 01:42:16 +0530

    Course Model Added & Controllers Updated

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js"
+import courseRoute from './routes/course.route.js'
 
 dotenv.config({})
 
@@ -24,6 +25,7 @@ app.use(cors({
 
 //apis
 app.use('/api/v1/user', userRoute);
+app.use('/api/v1/course', courseRoute);
 
 app.get("/home", (_, res) => {
     res.status(200).json({
