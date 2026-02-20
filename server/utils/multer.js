b5ec1a7a3cd7e45294995916d876136219f74fa5
joyboy-byte <vyash5491@gmail.com>
2026-02-20 13:44:53 +0530

    no change

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -1,25 +1,4 @@
-// import multer from "multer";
-
-// const upload = multer({dest:"uploads/"});
-// export default upload
-
 import multer from "multer";
-import path from "path";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 500 * 1024 * 1024, // 500MB
-  },
-});
-
+const upload = multer({dest:"uploads/"});
 export default upload;
